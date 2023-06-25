db9546667d9cdb39f6a80a7ebd07dbd66ead62c5
Brandon Guerra <104736190+BrGE09@users.noreply.github.com>
2023-06-25 00:05:30 -0600

    Inicio de plantilla

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resources/css/design.css">
+    <link rel="stylesheet" href="resources/bootstrap-5.0.0/css/bootstrap.css">
+    <link rel="stylesheet" href="resources/bootstrap-5.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="resources/bootstrap-5.0.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>Portafolio</title>
+</head>
+
+<body class="bg">
+    <div class="centro">
+        <div class="row">
+
+            <div class="col-sm-1"></div>
+
+            <div class="col-sm-3">
+                <div class="avatar p-4">
+                    <div class="perfil"></div>
+                    <button class="btn btns">Button</button>
+                </div>
+            </div>
+
+            <div class="col-sm-7">
+                <div class="presentacion p-4">
+                    <h1 class="nombre">NOMBRE EJEMPLO</h1>
+                    <h1 class="nombre">ÁREA</h1>
+                    <h1 class="nombre">COD<span style="color: #00FE8C">IM<span></h1>
+                </div>
+            </div>
+
+            <div class="col-sm-1"></div>
+        </div>
+    </div>
+</body>
+
+</html>
